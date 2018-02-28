459091bccf7619d014a478526e47bb8876c9cad0
Sylvain Beaubois <sylvain_beaubois@hotmail.fr>
2018-02-28 02:05:01 +0100

    Less is more

--- app/assets/javascripts/customed_js.js
+++ app/assets/javascripts/customed_js.js
@@ -1,3 +1,4 @@
+// code relatif à la tab
 function openRubric(evt, rubricName) {
     // Declare all variables
     var i, tabcontent, tablinks;
@@ -17,4 +18,49 @@ function openRubric(evt, rubricName) {
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(rubricName).style.display = "block";
     evt.currentTarget.className += " active";
+}
+
+// code relatif au slideshow
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1} 
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none"; 
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block"; 
+  dots[slideIndex-1].className += " active";
+}
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none"; 
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) {slideIndex = 1} 
+    slides[slideIndex-1].style.display = "block"; 
+    setTimeout(showSlides, 4000); // Change image every 5 seconds
 }
